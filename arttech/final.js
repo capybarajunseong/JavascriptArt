c28a1f8c6862c2d8f08f052b4632163beb22b264
capybarajunseong <junseong3646@gmail.com>
2025-06-10 20:45:51 +0900

    final.js 및 final.html 추가: 3개의 구슬이 파스텔 배경에 표시되는 페이지 구현

--- arttech/final.js
+++ arttech/final.js
@@ -1,44 +1,81 @@
-let fadeIn = 0;
-let textY = 0;
-let startTime;
+let marbles = [];
+let pastelColors = ['#FFB3B3', '#A5D8FF', '#FFF5A5']; // 파스텔 빨강, 파랑, 노랑 순서
+let backgroundImage;
+
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    textAlign(CENTER, CENTER);
-    startTime = millis();
-}
+  createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES);
 
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
+  // 구슬 초기화
+  marbles = [
+    {
+      x: width * 0.3,
+      y: height * 0.5,
+      r: 40,
+      color: pastelColors[1], // 하늘색
+      rotation: 0
+    },
+    {
+      x: width * 0.5,
+      y: height * 0.5,
+      r: 40,
+      color: pastelColors[2], // 노란색
+      rotation: 0
+    },
+    {
+      x: width * 0.7,
+      y: height * 0.5,
+      r: 40,
+      color: pastelColors[0], // 빨간색
+      rotation: 0
+    }
+  ];
 }
 
 function draw() {
-    background(255, 240, 245);
-    
-    // 메시지들
-    push();
-    translate(width/2, height/2 + textY);
-    
-    // 권경은
-    fill(100, 100, 100, fadeIn);
-    textSize(min(windowWidth, windowHeight) * 0.04);
-    textStyle(BOLD);
-    text("권경은 - 감사합니다", 0, -min(windowHeight * 0.1, 60));
-    
-    // 김준성
-    text("김준성 - 감사합니다", 0, 0);
-    
-    // 윤채원
-    text("윤채원 - 감사합니다", 0, min(windowHeight * 0.1, 60));
-    pop();
-    
-    // 페이드인 애니메이션
-    if (fadeIn < 255) {
-        fadeIn += 2;
-    }
-    
-    // 텍스트 움직임 애니메이션
-    if (textY > -min(windowHeight * 0.05, 30)) {
-        textY -= 0.5;
-    }
+  // 배경 이미지 그리기
+  if (backgroundImage) {
+    image(backgroundImage, 0, 0, width, height);
+  } else {
+    background(245); // 기본 파스텔 배경색
+  }
+
+  // 구슬 그리기
+  for (let marble of marbles) {
+    drawMarble(marble);
+  }
+}
+
+function drawMarble(marble) {
+  push();
+  translate(marble.x, marble.y);
+  rotate(marble.rotation);
+  
+  // 구슬 그림자
+  noStroke();
+  fill(0, 0, 0, 30);
+  ellipse(2, 2, marble.r, marble.r);
+  
+  // 구슬 본체
+  fill(marble.color);
+  ellipse(0, 0, marble.r, marble.r);
+  
+  // 구슬 하이라이트
+  fill(255, 255, 255, 100);
+  ellipse(-marble.r/4, -marble.r/4, marble.r/3, marble.r/3);
+  
+  pop();
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  
+  // 구슬 위치 재조정
+  marbles[0].x = width * 0.3;
+  marbles[0].y = height * 0.5;
+  marbles[1].x = width * 0.5;
+  marbles[1].y = height * 0.5;
+  marbles[2].x = width * 0.7;
+  marbles[2].y = height * 0.5;
 }
